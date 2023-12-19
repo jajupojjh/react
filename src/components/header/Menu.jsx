985d77785801bb2ed03cd8f252ad8cb951b0ace5
jjh <jajupojj@naver.com>
2023-12-19 14:34:48 +0900

    해더영역만들기

---
+++ src/components/header/Menu.jsx
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import {headerMenus,searchKeyword} from '../../data/header';
+import { Link, useLocation } from 'react-router-dom';
+const Menu = () => {
+    const location = useLocation()
+    return (
+        <nav className='header__menu'>
+            <ul className='menu'>
+                {headerMenus.map((menu, key) => (
+                    <li key={key} className={location.pathname === menu.src ? 'active' : ''}>
+                        <Link to={menu.src}>
+                            {menu.icon}{menu.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <ul className='keyword'>
+                {searchKeyword.map((keyword, key) => (
+                    <li key={key}>
+                        <Link to={keyword.src}>
+                            {keyword.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Menu;
